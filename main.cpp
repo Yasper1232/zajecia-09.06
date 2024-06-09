33fee1b251538ec910b564a15d8d97f0a5447aef
unknown <252761@edu.p.lodz.pl>
2024-06-09 11:22:16 +0200

    zajecia 09.06.2024

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "point.h"
+
+    using namespace std;
+
+int main(){
+
+Point P1;
+
+P1.setName("kwadrat");
+
+P1.setXY(2,3);
+cout<<P1.pole();
+
+
+    return 0;
+}
---
+++ point.cpp
@@ -0,0 +1,28 @@
+//
+// Created by student on 09.06.2024.
+//
+#include <iostream>
+#include "point.h"
+
+void Point::setName(const std::string& name) {
+    m_name = name;
+}
+void Point::setXY(double x, double y) {
+    m_x = x;
+    m_y = y;
+}
+double Point::getX() {
+    return m_x;
+}
+double Point::getY() {
+    return m_y;
+}
+void Point::show() {
+    std::cout << "Point: " << m_name << "(" << m_x << ", " << m_y << ")" << std::endl;
+}
+
+double Point::pole() {
+
+    return m_x*m_y;
+
+}
